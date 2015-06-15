05c7dadcdd11fedf46cb9f4080c0a1b93bd1a02f
Bala <bvanum@aarp.org>
2015-06-15 10:30:47 -0400

    Fixed typos

--- chef-repo/solo.rb
+++ chef-repo/solo.rb
@@ -5,5 +5,5 @@ chefdir = '/root/devopsdemo'
 file_cache_path chefdir
 data_bag_path   File.join(chefdir, 'data_bags')
 cookbook_path   [ File.join(chefdir, 'site-cookbooks'),
-                  File.join(chef, 'cookbooks') ]
-role_path       File.join(chef, 'roles')
+                  File.join(chefdir, 'cookbooks') ]
+role_path       File.join(chefdir, 'roles')
